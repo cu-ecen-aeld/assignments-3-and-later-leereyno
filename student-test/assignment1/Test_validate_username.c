2c259012df206faef2c28b4b8b918f8b84353bfa
Lee Reynolds <leebreynolds@protonmail.com>
2023-10-29 22:49:32 +0000

    use TEST_ASSERT_EQUAL_STRING function
    Signed-off-by: Lee Reynolds <leebreynolds@protonmail.com>

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,13 @@
 */
 void test_validate_my_username()
 {
+	const char * coded_username = my_username();
+
+	const char * file_username = malloc_username_from_conf_file();
+
     /**
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+	TEST_ASSERT_EQUAL_STRING(coded_username,file_username);
 }
