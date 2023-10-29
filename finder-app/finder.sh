e84803dc68d517ea1f624e978f0c5b5bc776684e
Lee Reynolds <leebreynolds@protonmail.com>
2023-10-29 23:53:16 +0000

    add comments to shell scripts
    Signed-off-by: Lee Reynolds <leebreynolds@protonmail.com>

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
+# Blame : Lee Reynolds
+
 if [ $# -lt 2 ] ; then
 	echo -e "\nOne or both arguments are missing\n"
 	exit 1
 fi
 
-filesdir=$1
-searchstr=$2
+filesdir=$1		# The directory where we will be searching for files that contain our search string
+searchstr=$2	# The string we will be searching for
 
-matchcount=0
-
+matchcount=0	# Grand total of all matches found, assume there are no matches
 
 if [ ! -d $filesdir ] ; then
 	echo -e "\nThe directory \"$filesdir\" does not appear to exist\n"
 	exit 1
 fi
 
-MESSAGE="The number of files are X and the number of matching lines are Y"
-
+# How many files are contained within our search directory and its subdirectories
+# search explicitly for files, do not include subdirectories in our count
 FILECOUNT=$(find $filesdir -type f | wc -l)
 
+# Examine each file found within the $filesdir directory
 for each in $(find $filesdir -type f) ; do
+
+	# How many times does our search string appear in the file we're currently examining?
 	thismatch=$(grep -c $searchstr $each)
+
+	# add $thismatch to our grand total for all matches found
 	matchcount=$(( matchcount + thismatch ))
 done
-
 
 echo "The number of files are $FILECOUNT and the number of matching lines are $matchcount"
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# Blame : Lee Reynolds
+
 if [ $# -lt 2 ] ; then
 	echo -e "\nOne or both arguments are missing\n"
 	exit 1
 fi
 
-writefile=$1
-writestr=$2
+writefile=$1		# The full path to a file that we'll be writing to (e.g. /tmp/this/subdir/filename.txt)
+writestr=$2			# String we will be writing into this file, overwriting anything already in the file
 
-filedir=$(dirname $writefile)
+filedir=$(dirname $writefile) # What is the path to our file? (e.g. /tmp/this/subdir )
 
-[ -d $filedir ] || mkdir -p $filedir
+[ -d $filedir ] || mkdir -p $filedir # If the directory path does not exist, then create it
 
-echo $writestr > $writefile
+echo $writestr > $writefile	# Save our string out to our file
 
 if [ $? -ne 0 ] ; then
 	echo -e "\nCannot write file \"$writefile\"\n"
